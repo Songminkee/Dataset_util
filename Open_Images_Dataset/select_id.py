f289a4d6f95f7b13e6ef30f8c0c00726cd480bdf
songminkee <thdalsrl10@gmail.com>
2020-11-26 00:31:42 +0900

    label extract add

--- Open_Images_Dataset/select_id.py
+++ Open_Images_Dataset/select_id.py
@@ -28,11 +28,14 @@ def move_file(args):
     print('Select image of box')
     dst=[]
     for id in info.keys():
-        flag=True
+        flag=False
         for ifo in info[id]:
-            if ifo[1] != want_id:
-                flag=False
-                break
+            if ifo[1] == want_id: # 1: LabelName
+                if ifo[11]=='0' and ifo[9]=='0': # 9: IsGroupOf , 11: IsInside
+                    flag=True
+                else:
+                    flag=False
+                    break
         if flag:
           dst.append(id)
 
@@ -41,14 +44,21 @@ def move_file(args):
         os.makedirs(dst_img_folder)
     if not os.path.exists(dst_label_foler):
         os.makedirs(dst_label_foler)
-
+    print('Find total {} data'.format(len(dst)))
     print('Move file from {} to {}'.format(image_folder,dst_img_folder))
 
     for id in dst:
         src_path = os.path.join(image_folder,id+'.jpg')
         subprocess.call(['mv',src_path,dst_img_folder])
-
-    # TODO: extract bounding box info:
+        with open(os.path.join(dst_label_foler,id+'.txt'),'w') as f:
+            for ifo in info[id]:
+                if ifo[1] != want_id:
+                    continue
+                x_min,x_max,y_min,y_max = ifo[3:7]
+                x_min, x_max, y_min, y_max = float(x_min),float(x_max),float(y_min),float(y_max)
+                cx,cy = (x_min+x_max)/2,(y_min+y_max)/2
+                w,h = (x_max-x_min),(y_max-y_min)
+                f.write('{} {} {} {}\n'.format(cx,cy,w,h))
 
     print('Done!')
 
@@ -57,11 +67,11 @@ if __name__=='__main__':
     parser.add_argument('--id', type=str, help='id you want extract', default='/m/025dyy')
     parser.add_argument('--annotation_file', type=str, help='oidv6-train-annotations-bbox.csv file path', default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/oidv6-train-annotations-bbox.csv')
     parser.add_argument('--image_folder', type=str, help='folder of data image',
-                        default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/train_1')
+                        default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/train_7')
     parser.add_argument('--dst_img_folder', type=str, help='image folder you want save',
-                        default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/box_img')
+                        default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/images')
     parser.add_argument('--dst_label_foler', type=str, help='label folder you want save',
                         default='/home/aiffel0042/Desktop/project/02.OBD,CD/object_dataset/labels')
-
+    # 0~3 다시
     args = parser.parse_args()
     move_file(args)
